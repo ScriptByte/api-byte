e32a610fce8902d2e1d383d75c13632b1f91fd99
Jatinder Singh <j.singh.developer@gmail.com>
2013-01-23 00:34:43 -0500

    Added comment and removed unused variable

--- app/scripts/directives/whenScrolled.js
+++ app/scripts/directives/whenScrolled.js
@@ -1,10 +1,13 @@
 'use strict';
 
+/**
+ * Directive for infinite scroll on a web page
+ *
+ * @author ScriptByte on 23/01/2013 at 12:33 AM
+ */
 apiByteApp.directive('whenScrolled', function() {
   return {
     link: function postLink(scope, element, attrs) {
-      var raw = element[0];
-
       window.onscroll = function() {
         if(window.innerHeight + document.body.scrollTop >= document.body.offsetHeight) {
           scope.$apply(attrs.whenScrolled);
