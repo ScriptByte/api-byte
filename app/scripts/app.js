b0889a1fc4cd329f4df62aa58f6742331f4df3b3
Jatinder Singh <j.singh.developer@gmail.com>
2013-01-22 22:27:51 -0500

    Twitter Bootstrap integration - #3

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var apiByteApp = angular.module('apiByteApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+apiByteApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
